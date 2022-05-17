9ed182f66618d11c351c365ce214b675673c940b
rickdevera <44529212+rickdevera@users.noreply.github.com>
2022-05-16 21:49:01 -0500

    added security group

---
+++ security_group.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "demo-security-group" {
+  name        = "Demo Security Group"
+  description = "Enable HTTP/HTTPS access on Port 80/443"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "HTTP Access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTPS Access"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description     = "SSH 22 access"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks     = ["10.0.1.0/24"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "Demo Security Group"
+  }
+}
