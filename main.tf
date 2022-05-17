d182083e167c8ec2538b7ce798399560645c545f
rickdevera <44529212+rickdevera@users.noreply.github.com>
2022-05-17 08:31:57 -0500

    add alias to provider

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-   region = "us-east-2"
- }
-
 #Create the VPC
  resource "aws_vpc" "Main" {                # Creating VPC here
    cidr_block       = var.main_vpc_cidr     # Defining the CIDR block use 10.0.0.0/24 for demo
---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+   region = "us-east-2"
+ }
+provider "aws" {
+   region = "us-east-2"
+   alias = "us-east"
+ }
