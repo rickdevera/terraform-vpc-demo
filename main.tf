fa940dfc0f7696fde054e8baaedeba28b0399af4
rickdevera <44529212+rickdevera@users.noreply.github.com>
2022-05-17 07:45:58 -0500

    changed provider in main

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+   region = "us-east-2"
+ }
+
 #Create the VPC
  resource "aws_vpc" "Main" {                # Creating VPC here
    cidr_block       = var.main_vpc_cidr     # Defining the CIDR block use 10.0.0.0/24 for demo
